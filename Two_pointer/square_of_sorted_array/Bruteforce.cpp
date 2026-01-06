a8389a392e665dd6aa442b3f796379ee8b983ad7
pranav verma <vermapranav001@gmail.com>
2026-01-06 23:43:07 +0530

    solved squares of sorted array leetcode 977

---
+++ Two_pointer/square_of_sorted_array/Bruteforce.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        // Step 1: Square each element
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        // Step 2: Sort the array
+        sort(nums.begin(), nums.end());
+
+        return nums;
+    }
+};
