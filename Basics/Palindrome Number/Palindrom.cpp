56639413d81f829156e200b6231975bfffcaa609
pranav verma <vermapranav001@gmail.com>
2026-01-22 00:05:25 +0530

    palindrom number problem

---
+++ Basics/Palindrome Number/Palindrom.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int original = n;
+    int rev = 0;
+
+    while (n > 0) {
+        int lastDigit = n % 10;
+        rev = rev * 10 + lastDigit;
+        n = n / 10;
+    }
+
+    if (original == rev)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
